b1c4783e276eca84b2d2f51e6e1da2180156316f
Mike Hall <mcsyko@gmail.com>
2018-12-22 22:27:19 +0200

    menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -10,11 +10,25 @@ public class Menu : MonoBehaviour
 {
     void Start()
     {
-
+        AspectTweak();
     }
 
     void Update()
     {
+    }
+
+    void AspectTweak()
+    {
+        if (Camera.main.aspect >= 1.7)
+            Camera.main.orthographicSize = 9;
+        else if (Camera.main.aspect >= 1.6)
+            Camera.main.orthographicSize = 10;
+        else if (Camera.main.aspect >= 1.5)
+            Camera.main.orthographicSize = 11;
+        else if (Camera.main.aspect >= 1.3)
+            Camera.main.orthographicSize = 12;
+        else
+            Camera.main.orthographicSize = 13;
     }
 
     public void Play()
