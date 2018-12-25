4eb227a14e02b3a8808005909511a7cb2f37c683
Mike Hall <mcsyko@gmail.com>
2018-12-25 17:20:25 +0200

    add birds

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour
+{
+    Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        StartCoroutine(Decay());
+    }
+
+    void Update()
+    {
+        body.MovePosition(body.position + (body.transform.forward * 0.15f));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+    }
+
+    IEnumerator Decay()
+    {
+        yield return new WaitForSeconds(20f);
+        Destroy(gameObject);
+    }
+}
